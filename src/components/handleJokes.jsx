cf661e61560073536611d89ee9300b1f1fc0002d
Asif Zawad <57825505+LaB1B@users.noreply.github.com>
2021-09-09 21:50:54 +0600

    Update handleJokes.jsx
    Updated the button to reload the whole page when punchline is rendered

--- src/components/handleJokes.jsx
+++ src/components/handleJokes.jsx
@@ -5,16 +5,20 @@ class HandleJokes extends Component {
     state = {
         Joke : {},
         buttonClicked : false,
-        punchLine : ""
+        punchLine : "",
+        punchLineGiven : false
     }
 
     GenerateJoke = () => {
         const joke = Jokes[parseInt(Math.random() * Jokes.length)]
         this.setState({Joke : joke , buttonClicked : true})
     }
+    handleReload = () => {
+        window.location.reload()
+    }
 
     RenderPunchLine = () => {
-        this.setState({punchLine : this.state.Joke.punchline})
+        this.setState({punchLine : this.state.Joke.punchline , punchLineGiven : true})
     }
     RenderJoke() {
         if (this.state.Joke.length === 0 ) {
@@ -31,27 +35,31 @@ class HandleJokes extends Component {
 
     render() {
         return (
-          <div className="theme" style = {{backgroundColor : this.props.GIF.color}}>
+          <div
+            className="theme"
+            style={{ backgroundColor: this.props.GIF.color }}
+          >
             <img
               width="440px"
               src={require(`../GIF/${this.props.GIF.path}`).default}
               alt=""
             />
-            <div className = "Jokes w-75">
-
-            <button
-              className={`btn btn-${this.props.GIF.button}  w-25 m-4`}
-              onClick={
+            <div className="Jokes w-75">
+              <button
+                className={`btn btn-${this.props.GIF.button}  w-25 m-4`}
+                onClick={
                   this.state.buttonClicked === false
-                  ? this.GenerateJoke
-                  : this.RenderPunchLine
+                    ? this.GenerateJoke
+                    : (this.state.punchLineGiven === false ? this.RenderPunchLine : this.handleReload)
                 }
-                >
-              {this.state.buttonClicked === false ? "Spit a Joke" : "Punchline"}
-            </button>
-            {this.RenderJoke()}
-            <p className = "punchline">{this.state.punchLine}</p>
-                </div>
+              >
+                {this.state.buttonClicked === false
+                  ? "Spit a Joke"
+                  : (this.state.punchLineGiven === false ? "Get Punchline" : "more....")}
+              </button>
+              {this.RenderJoke()}
+              <p className="punchline">{this.state.punchLine}</p>
+            </div>
             <p className="sideNote">Refresh the Page for more jokes</p>
           </div>
         );
